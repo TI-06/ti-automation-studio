b9fd74527b196260046ed8fc4da247a03d505a4e
TI-06 <iretaku.shop@gmail.com>
2026-08-26 15:02:56 +0900

    feat: enable sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({ session: false }),
+  integrations: [sitemap()],
   site: 'https://ti-automation-studio.workers.dev',
 });
